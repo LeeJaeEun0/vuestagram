d6ce82f435ed6df544e7848362076802b7681bbb
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-11-09 16:46:05 +0900

    progressive web 세팅

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import mitt from 'mitt'
 let emitter = mitt();
 let app = createApp(App)
 app.config.globalProperties.emitter = emitter;
+
 // 자주 사용하는 라이브러리 추가하기
 
 import store from './store.js'
+
+import './registerServiceWorker'
 app.use(store).mount('#app')
